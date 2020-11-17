260c49bf7181f7adaa2d8c1a2370368ae7e8bdc0
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-11-17 10:51:08 -0500

    Add new styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Matter from 'matter-js'
+import React from "react";
+import ReactDOM from "react-dom";
+import Matter from "matter-js";
 
-import './App.css'
+import "./App.css";
 
-import logo from './images/Logo.png'
-import plus1 from './images/test.png'
-import plus2 from './images/02.png'
-import plus3 from './images/03.png'
-import plus4 from './images/04.png'
-import plus5 from './images/05.png'
-import plus6 from './images/06.png'
+import logo from "./images/Logo.png";
+import plus1 from "./images/test.png";
+import plus2 from "./images/02.png";
+import plus3 from "./images/03.png";
+import plus4 from "./images/04.png";
+import plus5 from "./images/05.png";
+import plus6 from "./images/06.png";
 
 class Scene extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   componentDidMount() {
@@ -27,28 +27,22 @@ class Scene extends React.Component {
       Bodies = Matter.Bodies,
       Constraint = Matter.Constraint,
       Mouse = Matter.Mouse,
-      MouseConstraint = Matter.MouseConstraint
+      MouseConstraint = Matter.MouseConstraint;
 
     var engine = Engine.create({
-      // positionIterations: 20
-    })
 
-    let width = window.innerWidth
-    let height = window.innerHeight
-    let ballCount = 12
-    var ballSize = height / 8
-    var smallBall = height / 250
-    var bigBall = height / 40
-    var color1 = 'rgba(255,255,255,1)'
-    var color2 = 'rgba(250,255,17,1)'
-    var color3 = 'black'
-    var color4 = 'whitesmoke'
-    var color5 = 'red'
-    var color6 = 'bisque'
-    var redColor = 'red',
-      greenColor = 'blue'
-    let raf = null
-    let bodyRotation = 0
+    });
+
+    let width = 600;
+    let height = 600;
+    let ballCount = 12;
+    var ballSize = height / 8;
+    var color3 = "transparent";
+    let raf = null;
+    let bodyRotation = 0;
+
+    var rotateCategory = 0x0001,
+      boundaryCategory = 0x0002;
 
     var render = Render.create({
       element: this.refs.scene,
@@ -57,24 +51,57 @@ class Scene extends React.Component {
         width: width,
         height: height,
         wireframes: false,
-        background: 'transparent',
-        showVelocity: false,
-        showDebug: false,
-        showBroadphase: false,
-        showBounds: false,
-        showCollisions: false,
-        showSeparations: false,
-        showAxes: false,
-        showPositions: false,
         showAngleIndicator: false,
-        showIds: false,
-        showShadows: false,
-        showVertexNumbers: false,
-        showConvexHulls: false,
-        showInternalEdges: false,
-        showMousePosition: false,
+        showCollisions: false,
+        showVelocity: false,
       },
-    })
+    });
+    Render.run(render);
+
+    const w = 20;
+    const extraLength = 1;
+    const initialRotation = 0;
+    const sides = 100;
+    const radius = 280;
+    const options = {};
+    const theta = (2 * Math.PI) / sides;
+    const sideLength = ((2 * radius * theta) / 2) * extraLength;
+
+    const parts = [];
+    for (let i = 0; i < sides; i++) {
+      // We'll build thin sides and then translate + rotate them appropriately.
+      const body = Bodies.rectangle(0, 0, sideLength, w, {
+        isStatic: true,
+        collisionFilter: {
+          category: boundaryCategory,
+        },
+        render: {
+          fillStyle: "transparent",
+        },
+      });
+      Body.rotate(body, i * theta);
+      Body.translate(body, {
+        x: radius * Math.sin(i * theta),
+        y: -radius * Math.cos(i * theta),
+      });
+      parts.push(body);
+    }
+    const ret = Body.create({
+      isStatic: true,
+      collisionFilter: {
+        category: boundaryCategory,
+      },
+      render: {
+        fillStyle: "transparent",
+      },
+    });
+    Body.setParts(ret, parts);
+    // if (initialRotation) {
+    //   Body.rotate(ret, initialRotation);
+    // }
+    Body.translate(ret, { x: 300, y: 300 });
+
+    World.add(engine.world, [ret]);
 
     // add mouse control
     var mouse = Mouse.create(render.canvas),
@@ -86,24 +113,32 @@ class Scene extends React.Component {
             visible: false,
           },
         },
-      })
+      });
 
-    World.add(engine.world, mouseConstraint)
+    mouseConstraint.collisionFilter.mask = rotateCategory;
+
+    World.add(engine.world, mouseConstraint);
 
     var sstop1 = Bodies.circle(width / 4.2, (height / 5) * 3.8, height / 20, {
       isStatic: true,
-      render: {
-        fillStyle: 'transparent',
+      collisionFilter: {
+        category: rotateCategory | boundaryCategory,
       },
-    })
-    World.add(engine.world, [sstop1])
+      render: {
+        fillStyle: "transparent",
+      },
+    });
+    World.add(engine.world, [sstop1]);
 
     for (let i = 0; i < ballCount; i++) {
-      let radius = Math.round(30 + (Math.random() * height) / 25)
+      let radius = Math.round(30 + (Math.random() * height) / 25);
       var ball = Bodies.circle(width / 4, height / 6, radius, {
         density: 0.04,
         frictionAir: 0.06,
         restitution: 0.8,
+        collisionFilter: {
+          category: rotateCategory | boundaryCategory,
+        },
         render: {
           sprite: {
             texture: [plus1, plus2, plus3, plus4, plus5, plus6][
@@ -116,65 +151,53 @@ class Scene extends React.Component {
           strokeStyle: color3,
           lineWidth: 1,
         },
-      })
+      });
       var constraint = Constraint.create({
         pointA: { x: width / 2, y: height / 2 },
         bodyB: ball,
         length: ballSize,
         stiffness: 0.0001,
         render: { visible: false },
-      })
-      World.add(engine.world, [ball, constraint])
+      });
+      World.add(engine.world, [ball, constraint]);
     }
 
-    var body = Bodies.rectangle(200, 20, 300, 500, {
-      render: {
-        fillStyle: 'transparent',
-      },
-    })
-    var constraint2 = Constraint.create({
-      pointA: { x: width / 2, y: height / 2 },
-      bodyB: body,
-      length: ballSize,
-      stiffness: 0.0001,
-      render: { visible: false },
-    })
-    World.add(engine.world, [body, constraint2])
-
-    let textBlock = this.refs.info
+    // let textBlock = this.refs.info
 
     var w1 = Bodies.rectangle(width / 2, height / 2, 100, 2, {
       isStatic: true,
-      render: {
-        fillStyle: 'transparent',
+      collisionFilter: {
+        category: rotateCategory,
       },
-    })
+      render: {
+        fillStyle: "transparent",
+      },
+    });
 
-    World.add(engine.world, [w1])
-    Body.rotate(w1, bodyRotation)
+    World.add(engine.world, [w1]);
+    Body.rotate(w1, bodyRotation);
 
-    engine.world.gravity.y = 0
+    engine.world.gravity.y = 0;
 
-    Engine.run(engine)
-    Render.run(render)
+    Engine.run(engine);
+    Render.run(render);
 
     function update() {
-      console.log(body.position.x)
-      bodyRotation = bodyRotation + 0.00001
-      Body.rotate(w1, bodyRotation)
+      bodyRotation = bodyRotation + 0.00001;
+      Body.rotate(w1, bodyRotation);
 
-      textBlock.style.transform =
-        'translate( ' +
-        body.position.x / 2 +
-        'px, ' +
-        body.position.y / 2 +
-        'px )'
-      textBlock.style.transform += 'rotate( ' + body.angle / 4 + 'rad )'
-      raf = requestAnimationFrame(update.bind(this))
+      // textBlock.style.transform =
+      //   'translate( ' +
+      //   body.position.x / 2 +
+      //   'px, ' +
+      //   body.position.y / 2 +
+      //   'px )'
+      // textBlock.style.transform += 'rotate( ' + body.angle / 4 + 'rad )'
+      raf = requestAnimationFrame(update.bind(this));
     }
-    update()
-    var runner = Runner.create()
-    Runner.run(runner, engine)
+    update();
+    var runner = Runner.create();
+    Runner.run(runner, engine);
   }
 
   render() {
@@ -183,20 +206,52 @@ class Scene extends React.Component {
         <div className="info">
           <img ref="logo" className="logo" alt="Suprlus+ logo" src={logo} />
           <p ref="info">
-            As an anti-capitalist creative business center, our practice is
-            mission-driven and we think and make as a collective. We are
-            streeeeeetchy, and we seek to work against the reproduction of harm
-            by redistributing wealth and labor. For us, advocating for Black
-            lives matter also means supporting Black creativity and expressions,
-            therefore a percentage of every payment we receive will go towards
-            fully subsidizing studio space(s) in Soft Surplus for Black
-            artist(s). You can learn more about our redistribution plan by
-            visiting the Surplus+ financial report.{' '}
+            Surplus+ (“sur-plus-plus”) is an elastic, multi-disciplinary team of
+            soft workers offering <span className="pink">art</span>,{" "}
+            <span className="green">design</span>,{" "}
+            <span className="yellow">research</span> and{" "}
+            <span className="blue">production</span> services to you. Surplus+
+            is born out of a desire to financially sustain our collective studio
+            space, Soft Surplus, support our members under COVID-19, as well as
+            our love of learning from and working with each other.
+            <br />
+            <br />
+            Our practice is mission-driven:
+            <ul>
+              <li>we think and make as a collective</li>
+              <li>we actively seek to redistribute wealth and labor</li>
+              <li>we work with clients whose values we align with</li>
+            </ul>
+          </p>
+
+          <section className="rolodex">
+            <button className="button">hire us</button>
+            <div>
+              <div className="slidingVertical">
+                <span>+ writing + financial modeling + chatbots/ML apps</span>
+                <span>+ curriculum design + cooking + graphic design</span>
+                <span>
+                  + interactive design tools + furniture + 3D modeling
+                </span>
+                <span>+ architecture + digital fabrication + film editing</span>
+                <span>+ gardening + web design/dev + dancing + drawing</span>
+                <span>+ UI/UX + physical computing + wireless sensors</span>
+                <span>+ print design + inflatables + carpentry + more!</span>
+              </div>
+            </div>
+          </section>
+          <p className="small">
+            A percentage of every payment we receive will go towards subsidizing
+            studio spaces in Soft Surplus for Black members and towards Black
+            led organizations we love & support. The goal of Surplus+ is to
+            create structures for reducing barriers to access and participation
+            in creative community spaces. We are designing our bylaws to become
+            a worker-owned cooperative in the State of New York.
           </p>
         </div>
-        <div ref="scene" style={{ width: '100%', height: '100%' }} />
+        <div ref="scene" className="canvas" />
       </div>
-    )
+    );
   }
 }
-export default Scene
+export default Scene;
